f7e9bbd89b043bed4e1a339c7fc525850e939cc7
john <nergilz@yandex.ru>
2018-03-08 13:15:13 +0200

    first version

--- README.md
+++ README.md
@@ -1,7 +1,46 @@
-# Frequency Analysis of Words
+## Frequency Analysis of Words
 
-[TODO. There will be project description]
+This script make frequency analysis of words in text
+---
 
-# Project Goals
+### discription of the script
++ specify the path to start
++ the script will print ten words and their number
++ you can change the number of words
+
+#### change the number of words in function:
+```python
+def get_most_frequent_words(dict_words)
+```
+```python
+for i in range(<your number>):
+```
+
+### script start example
+```bash
+python3 lang_frequency.py arch/wp.txt
+```
+
+### example result
+```bash
+the : 148
+you : 93
+and : 88
+to : 70
+i : 69
+a : 49
+in : 48
+he : 47
+it : 45
+your : 44
+
+```
+
+### requirements for running
+```bash
+pthon 3.5 ver
+```
+---
+## Project Goals
 
 The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,54 @@
+import sys
+import string
 
 
-def load_data(filepath):
-    pass
+exclude = list(string.punctuation)
+exclude.append(chr(8211))
 
 
-def get_most_frequent_words(text):
-    pass
+def load_data(file_path):
+    dict_words = {}
+    with open(file_path, 'r') as file_handler:
+        for line in file_handler:
+            line = ''.join(i for i in line if i not in exclude).split()
+            for word in line:
+                word = word.lower()
+                dict_words[word] = dict_words.get(word, 0) + 1
+    return dict_words
 
+
+def get_most_frequent_words(dict_words):
+    dict_max_value = {}
+    for i in range(10):
+        key_max_value = (max(dict_words.keys(), key=lambda x: dict_words[x]))
+        max_value = dict_words.get(key_max_value)
+        dict_max_value.update({key_max_value: max_value})
+        dict_words.pop(key_max_value, max_value)
+    return dict_max_value
+
+
+def pprint_most_frequent_words(dict_max_value):
+    sorted_by_value = sorted(
+        dict_max_value,
+        key=lambda x: int(dict_max_value[x]),
+        reverse=True
+        )
+    for key in sorted_by_value:
+        print(key, ':', dict_max_value.get(key))
+
+
+def main():
+    try:
+        file_path = sys.argv[1]
+        dict_max_value = get_most_frequent_words(load_data(file_path))
+        pprint_most_frequent_words(dict_max_value)
+    except IndexError:
+        print(' Error: No filename for reading!\n')
+    except FileNotFoundError:
+        print(' Error: file or path "{0}" not found!\n'.format(file_path))
 
 if __name__ == '__main__':
-    pass
+    try:
+        main()
+    except Exception as ex:
+        print(' ERROR: {}\n'.format(ex))
